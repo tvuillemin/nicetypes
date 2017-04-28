b62f1333dbb12e2154f144cda07f2d7b38c20f61
Thibaut Vuillemin <contact@thibautvuillemin.com>
2017-04-28 23:57:20 +0200

    packaging: 1st draft

---
+++ src/nicetypes/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1.0"
+
+__title__ = "nicetypes"
+__description__ = "Advanced types for Python3"
+__uri__ = "https://github.com/tvuillemin/nicetypes"
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Thibaut Vuillemin"
+__email__ = "contact@thibautvuillemin.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 Thibaut Vuillemin"
